d4ad3f4b7e4c8d14378d179b70fe85a39daeb821
Dust397 <752139@student.dusd.net>
2024-02-29 11:35:48 -0800

    finish with main things need to add camera switcher and uncoming cars

--- P3PlayerControlRosales-Prototype1/Assets/Scripts/PlayerController.cs
+++ P3PlayerControlRosales-Prototype1/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,19 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+    private float forwardInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
